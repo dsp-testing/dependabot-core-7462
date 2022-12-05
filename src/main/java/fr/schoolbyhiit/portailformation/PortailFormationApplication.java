6cc217b99576fd54d2bc75f168fd20c7773719a1
mathieu verpillat <mathieu.verpillat@hiit-consulting.fr>
2022-12-05 17:11:49 +0100

    chore: project init

---
+++ src/main/java/fr/schoolbyhiit/portailformation/PortailFormationApplication.java
@@ -0,0 +1,13 @@
+package fr.schoolbyhiit.portailformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailFormationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortailFormationApplication.class, args);
+    }
+
+}
